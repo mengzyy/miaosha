b022862dab9f76582815b76d000bdfe3ca81d6eb
18306299232 <18306299232@163.com>
2020-07-19 17:26:20 +0800

    Initial commit

---
+++ src/main/java/com/mzy/miaosha/MiaoshaApplication.java
@@ -0,0 +1,16 @@
+package com.mzy.miaosha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author :Mengzy
+ */
+@SpringBootApplication
+public class MiaoshaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiaoshaApplication.class, args);
+    }
+
+}
