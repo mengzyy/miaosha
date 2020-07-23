522ea28201f9fd7b493bc122616acaa5a70db2ee
mengzhongyi <18306299232@163.com>
2020-07-23 18:26:58 +0800

    用户验证init

--- src/main/java/com/mzy/miaosha/controller/SampleController.java
+++ src/main/java/com/mzy/miaosha/controller/SampleController.java
@@ -4,17 +4,12 @@ package com.mzy.miaosha.controller;
 import com.mzy.miaosha.domin.User;
 import com.mzy.miaosha.redis.RedisService;
 import com.mzy.miaosha.redis.UserKey;
-
 import com.mzy.miaosha.result.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 @RequestMapping("/demo")
